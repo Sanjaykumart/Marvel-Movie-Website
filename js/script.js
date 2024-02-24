d4a81dfb4eb5f129130fa904ead5c2dce6cccb0a
Sanjaykumart <sanjaykumarsanjaykumar8286@gmail.com>
2024-02-24 13:47:48 +0530

    fix : Uploaded  my all folder and files in my new repository

---
+++ js/script.js
@@ -0,0 +1,53 @@
+/**
+ * add event on multiple elements
+ */
+
+const addEventOnElements = function (elements, eventType, callback) {
+  for (let i = 0, len = elements.length; i < len; i++) {
+    elements[i].addEventListener(eventType, callback);
+  }
+}
+
+/**
+ * MOBILE NAVBAR
+ * navbar will show after clicking menu button
+ */
+
+const navbar = document.querySelector("[data-navbar]");
+const navToggler = document.querySelector("[data-nav-toggler]");
+const navLinks = document.querySelectorAll("[data-nav-link]");
+
+const toggleNav = function () {
+  navbar.classList.toggle("active");
+  this.classList.toggle("active");
+}
+
+navToggler.addEventListener("click", toggleNav);
+
+const navClose = () => {
+  navbar.classList.remove("active");
+  navToggler.classList.remove("active");
+}
+
+addEventOnElements(navLinks, "click", navClose);
+
+/**
+ * Scroll reveal
+ */
+
+const revealElements = document.querySelectorAll("[data-reveal]");
+
+const revealElementOnScroll = function () {
+  for (let i = 0, len = revealElements.length; i < len; i++) {
+    const isElementInsideWindow = revealElements[i].getBoundingClientRect().top < window.innerHeight / 1.1;
+
+    if (isElementInsideWindow) {
+      revealElements[i].classList.add("revealed");
+    }
+  }
+}
+
+window.addEventListener("scroll", revealElementOnScroll);
+
+window.addEventListener("load", revealElementOnScroll);
+
